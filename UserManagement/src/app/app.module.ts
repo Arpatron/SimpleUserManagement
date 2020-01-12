e4fdb8e9e97e24bc666f5278e7cfd00203430923
Arpatron <cruiloba@gmail.com>
2020-01-12 16:16:46 +0100

    modules and core empty

--- UserManagement/src/app/app.module.ts
+++ UserManagement/src/app/app.module.ts
@@ -3,10 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { UserCreateComponent } from './components/users/user-create/user-create.component';
+import { UserDeleteComponent } from './components/users/user-delete/user-delete.component';
+import { UserEditComponent } from './components/users/user-edit/user-edit.component';
+import { UserDetailComponent } from './components/users/user-detail/user-detail.component';
+import { UserListComponent } from './components/users/user-list/user-list.component';
+import { PageNotFoundComponent } from './components/others/page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    UserCreateComponent,
+    UserDeleteComponent,
+    UserEditComponent,
+    UserDetailComponent,
+    UserListComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
---
+++ UserManagement/src/app/core/models/states.enum.ts
@@ -0,0 +1,2 @@
+export enum States {
+}
